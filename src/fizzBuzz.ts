28c39e2eef873a60190cd2aee61768494a87a5b7
Rudolph Jose Razul <rudolphjose.razul@timesoftsg.com.sg>
2024-05-21 17:56:24 +0800

    updated code

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,15 +1,17 @@
-export function fizzBuzz(n: number): string[] {
-    const result: string[] = [];
-    for (let i = 1; i <= n; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            result.push("FizzBuzz");
-        } else if (i % 3 === 0) {
-            result.push("Fizz");
-        } else if (i % 5 === 0) {
-            result.push("Buzz");
-        } else {
-            result.push(i.toString());
-        }
-    }
-    return result;
+/*
+Fizz buzz is a group word game for children to teach them about division. 
+
+Write a function fizzBuzz(n: number): string[] that returns an array of strings representing the numbers from 1 to n, but with 
+    multiples of 3 replaced by "Fizz", 
+    multiples of 5 replaced by "Buzz", and 
+    multiples of both 3 and 5 replaced by "FizzBuzz".
+
+eg 
+n = 15
+should display: ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
+*/
+
+export function fizzBuzz(n: number): string[] {   
+    //your code here
+    return [];
 }
