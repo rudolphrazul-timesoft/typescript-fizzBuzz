4c0e3c44e4393b0ff4cde5f9ea7f9a8b14966c76
Rudolph Jose Razul <rudolphjose.razul@timesoftsg.com.sg>
2024-05-21 14:54:26 +0800

    init code

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { fizzBuzz } from "./fizzBuzz";
+
+console.log("\nFizzBuzz:");
+console.log(fizzBuzz(15)); // ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
